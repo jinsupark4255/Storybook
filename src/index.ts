3aae4c618f6d8fed443d0e4b55051113ee5c4859
jinsupark4255 <jin04007@naver.com>
2025-04-27 16:46:27 +0900

    add: npm registry에 빌드 후 배포 준비 완료 (token 제외)

---
+++ src/index.ts
@@ -0,0 +1,2 @@
+export { Button } from "./Button"; // 버튼 컴포넌트
+export * from "./Button/constants"; //버튼 컴포넌트 관련 상수들
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    dts({ include: ['src'] })
+  ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'MyComponentLibrary',
+      formats: ['es', 'cjs'],
+      fileName: (format) => `index.${format === 'es' ? 'es.js' : 'js'}`
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    }
+  }
+});
